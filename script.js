0caf75d229793a7eb6e572fac77491e06ecd10ad
Arka <arkamazumder0@gmail.com>
2022-04-27 10:12:47 +0530

    Game Logic for again btn

--- script.js
+++ script.js
@@ -39,3 +39,12 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.number').style.width = '30rem';
   }
 });
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.guess').value = '';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+});
